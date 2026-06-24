feb8973686abf7796b3fdbab6ad56792dcdfebd4
Roberto <robertoo.jof@gmail.com>
2026-06-24 15:45:43 -0300

    feat: implementa primeira versão do compressor (puro/sem poda e sem reset)

--- src/compressor/compressor.hpp
+++ src/compressor/compressor.hpp
@@ -1,14 +1,24 @@
 #pragma once
+
+#include "ArithmeticCoder.hpp"
+#include "PatriciaTree.hpp"
+
+#include <fstream>
+#include <set>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Compressor {
+private:
+  void encoder_pure(std::string &message,
+                    const std::string &compressed_filename, int kmax);
+  void encoder_reset();
+  void encoder_pode();
+
 public:
-  void encoder_with_reset(string &message,
-                                 const string &compressed_filename,
-                                 const string &log_filename, int kmax,
-                                 size_t window_size, double threshold);
+  void encode();
   void decode_reset(const string &compressed_filename,
-                           const string &output_filename);
+                    const string &output_filename);
 };
