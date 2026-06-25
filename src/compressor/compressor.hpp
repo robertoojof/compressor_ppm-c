7dba5fdee18ee5dec5844e1298b80eb929dcc801
Roberto <robertoo.jof@gmail.com>
2026-06-25 11:16:57 -0300

    refat: altera visibilidade de métodos na classe Compressor

--- src/compressor/compressor.hpp
+++ src/compressor/compressor.hpp
@@ -11,13 +11,9 @@
 using namespace std;
 
 class Compressor {
-private:
+public:
   void encoder_pure(std::string &message,
                     const std::string &compressed_filename, int kmax);
-  // void encoder_reset();
-  // void encoder_pode();
-
-public:
   void encode();
   void decode_pure(const string &compressed_filename,
                     const string &output_filename);
