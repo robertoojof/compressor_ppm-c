7e54d01eb288b1557677d2b3d100ccb174911f05
Roberto <robertoo.jof@gmail.com>
2026-06-25 22:20:21 -0300

    fix: remove comentário redundante na função de leitura de arquivo

--- src/fileIO/ReadData.cpp
+++ src/fileIO/ReadData.cpp
@@ -8,8 +8,6 @@ void ReadData::readFile(string &message, const string &filename) {
   ifstream file(filename, ios::binary);
   if (file.is_open()) {
     // obter tamanho do arquivo, movemos o ponteiro para o final do arquivo
-
-    // obter tamanho do arquivo, movemos o ponteiro para o final do arquivo
     file.seekg(0, ios::end);
     size_t fileSize = file.tellg();
     file.seekg(0, ios::beg);
